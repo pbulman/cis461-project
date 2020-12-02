645124ae19d33adcff15dd1cb7312e9265e617c1
pbulman <45780106+pbulman@users.noreply.github.com>
2020-12-02 14:11:49 -0500

    Update RollerCoaster.java

--- src/RollerCoaster.java
+++ src/RollerCoaster.java
@@ -1,24 +1,60 @@
-public class RollerCoaster {
-    
-    private boolean cond = true;
+package finalproj;
 
-    public synchronized void load(int val) throws InterruptedException{
-    	while(!cond) {
-            wait();
-        }
+public class BufferImp <E> implements Buffer <E>{
 	
-	//insert here
-    	
-        notifyAll();
-    }
+	protected E[] buf;
+	protected int in = 0;
+	protected int out = 0;
+	protected int count = 0;
+	protected int size;
+	
+	protected int max = 5;
+	protected int numPassengers = 0;
+	protected boolean left = false;
+	
+	@SuppressWarnings("unchecked")
+	public BufferImp(int size) {
+		this.size = size;
+		buf = (E[]) new Object[size];
+	}
+	
+	public synchronized void load() 
+            throws InterruptedException {
+	    while (numPassengers == max)  {
+	    	wait();
+	    }
+	    ++numPassengers;	    
+	    notifyAll();
+	}
 
-    public synchronized int unload() throws InterruptedException{
-        while(cond) {
-            wait();   
-        }
-	    
-	 //insert here
-
-        notifyAll();
-    }
+	public synchronized void unload() 
+            throws InterruptedException {
+	    while (numPassengers < max)  {
+	    	wait();
+	    }
+	    --numPassengers;	    
+	    notifyAll();
+	}
+	
+	public synchronized void ride() 
+            throws InterruptedException {
+	    while (numPassengers < max)  {
+	    	wait();
+	    }
+	    left = true;	    
+	}
+	
+	public synchronized void clean() 
+            throws InterruptedException {
+	    while (numPassengers < max || !left)  {
+	    	wait();
+	    }
+	    numPassengers = 0;
+	    left = false;	    
+	    notifyAll();
+	}
+	
+	
 }
+
+
