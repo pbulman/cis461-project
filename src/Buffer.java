455a3b2cace9c47274ea2f1f417c03700fa4dad5
pbulman <45780106+pbulman@users.noreply.github.com>
2020-12-03 16:42:58 -0500

    Delete Buffer.java

--- src/Buffer.java
+++
@@ -1,6 +0,0 @@
-public interface Buffer<E> {
-	//put object into buffer
-	public void put(E o) throws InterruptedException;
-	//get object from buffer
-	public E get() throws InterruptedException;
-}
